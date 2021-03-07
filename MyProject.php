650313d2560359a86b16bfbc1645d0d90c911c36
luqmanharis12 <79737286+luqmanharis12@users.noreply.github.com>
2021-03-07 22:33:08 +0700

    Add files via upload

---
+++ MyProject.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>luqman hakim hs</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="header">
+        <div class="navigator">
+            <div class="navigatorT">My Website</div>
+        </div>
+    </div>
+    <div class="tengah">
+        <div class="sidebar">
+            <a href="MyProject"><div class="bar" id="projek">My Project</div></a>
+            <a href="about"><div class="bar">About Me</div></a>
+            <a href="ask"><div class="bar">Ask Me!</div></a>
+        </div>
+        <div class="utama">
+         <div class="title">My Project</div>
+         <hr>
+         <div>
+            <table id="tabel">
+                <tr>
+                    <th>No</th>
+                    <th>Project</th>
+                    <th>As</th>
+                    <th>Description</th>
+                    </tr>
+                    <tr>
+                    <td>1</td>
+                    <td>Music Recomendation based on Face Expression Detection</td>
+                    <td>Data Analist</td>
+                    <td>Develop existing music applications by adding features to recommend music playlist based on playlists based on facial expressions by using facial detection.</td>
+                    </tr>
+                    <tr>
+                    <td>2</td>
+                    <td>Helpdesk Assistant</td>
+                    <td>Frontend</td>
+                    <td>it is a website used by the helpdesk to record customer complaint processes.</td>
+                </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Little Cop</td>
+                        <td>Game Dev</td>
+                        <td>In this research, a game called "Little Cop" was made. Where this game will be a public media in knowing the importance of traffic rules and what sanctions will be obtained when violating one or more of the existing regulations. </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+        <div class="footerin">
+        <a href="https://github.com/luqmanharis12/tugas2">Github</a>
+
+
+        </div>
+    </div>
+</body>
+</html>
